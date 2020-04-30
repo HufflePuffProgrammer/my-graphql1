23d978b4ada49f24e651e04eb741eb5ecf38b3d1
hufflepuffprogrammer <wdrum4food@yahoo.com>
2020-04-29 18:23:56 -0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-//Components
-import BookList from "./components/BookList";
-
-// apollo client setup
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-});
-
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <h1>1Harry Potter's reading list</h1>
-          <BookList />
-        </div>
-      </ApolloProvider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
